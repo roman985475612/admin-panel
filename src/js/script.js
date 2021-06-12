b92eb2706bf93142635fd7db6cc5cf99b7bd2c4d
Roman <roman985475612@gmail.com>
2021-06-12 18:09:51 +0300

    Chapter 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
-$( function() {
-    $( '.sidenav' ).sidenav()
-})
+$( '.section' ).hide()
+
+setTimeout(function() {
+    $( function() {
+        $( '.section' ).fadeIn()
+
+        $( '.loader' ).fadeOut()
+
+        $( '.sidenav' ).sidenav()
+    
+        $( '.count' ).each(function() {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 1000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now))
+                }
+            })
+        })
+    })
+    
+}, 1000)
+

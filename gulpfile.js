c80ec4107a030cbdfbb48a072f121048524a9541
Roman <roman985475612@gmail.com>
2021-06-12 17:32:08 +0300

    Chapter 2 completed

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp         = require('gulp')
+const del          = require('del')
+const concat       = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS     = require('gulp-clean-css')
+const sourcemaps   = require('gulp-sourcemaps')
+const browserSync  = require('browser-sync').create()
+const gulpIf       = require('gulp-if')
+const uglify       = require('gulp-uglifyjs')
+
+let isDev  = process.argv.includes('--dev')
+let isProd = !isDev
+
+function clean() {
+    return del('./build/*')
+}
+
+function html() {
+    return gulp.src('./src/**/*.html')
+               .pipe(gulp.dest('./build'))
+               .pipe(browserSync.stream())
+}
+
+function style() {
+    return gulp.src(['./src/css/**/*.css'])
+               .pipe(gulpIf(isDev, sourcemaps.init()))
+               .pipe(concat('main.css'))
+               .pipe(autoprefixer())
+               .pipe(gulpIf(isProd, cleanCSS({
+                   level: 2
+                })))
+               .pipe(gulpIf(isDev, sourcemaps.write('.')))
+               .pipe(gulp.dest('./build/css'))
+               .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return gulp.src([
+            './node_modules/jquery/dist/jquery.min.js',
+            './node_modules/materialize-css/dist/js/materialize.min.js',
+            './src/js/**/*.js'
+        ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.stream())
+}
+
+function image() {
+    return gulp.src('./src/img/**/*')
+               .pipe(gulp.dest('./build/img'))
+}
+
+function mStyle() {
+    return gulp.src('./node_modules/materialize-css/dist/css/materialize.min.css')
+               .pipe(gulp.dest('./build/css'))
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./build/"
+        }
+    })
+    gulp.watch('./src/**/*.html', html)
+    gulp.watch('./src/css/**/*.css', style)
+    gulp.watch('./src/js/**/*.js', scripts)
+}
+
+let build = gulp.parallel(html, style, scripts, image, mStyle)
+let buildWithClean = gulp.series(clean, build)
+let dev = gulp.series(buildWithClean, watch)
+
+gulp.task('build', buildWithClean)
+gulp.task('dev', dev)
